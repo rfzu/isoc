ebfe530b7e9572d94e421cc18fbe4a8cd9085f57
rfzu <rall@list.ru>
2014-08-05 19:59:39 +0400

    Make a basic User model (including secure passwords)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140805145658) do
+ActiveRecord::Schema.define(version: 20140805155551) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140805145658) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
