5a9261bef356d43b46c8b5b3bf647b4a9f9f1463
rfzu <rall@list.ru>
2014-08-24 01:11:39 +0400

    add user column to all lists post

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-  belongs_to :user
-  default_scope -> { order('created_at DESC') }
+  belongs_to :user, :foreign_key => :user_id
+  #default_scope -> { order('created_at DESC') }
 end
